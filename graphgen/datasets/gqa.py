725b9fc07073460478418023bea4e97e7a558780
Alex Mirrington <alexmirrington@gmail.com>
2020-07-01 14:02:03 +1000

    Move ChunkedJsonDataset to utilities module.

--- graphgen/datasets/gqa.py
+++ graphgen/datasets/gqa.py
@@ -1,10 +1,11 @@
-"""Module containing a torch-compatible GQA dataset implementation."""
-import json
+"""A torch-compatible GQA dataset implementation."""
 import os.path
 from enum import Enum
-from typing import Any, Dict, Tuple
+from typing import Any
 
 import torch.utils.data
+
+from .utilities import ChunkedJSONDataset
 
 
 class GQASplit(Enum):
@@ -22,91 +23,6 @@ class GQAVersion(Enum):
 
     BALANCED = "balanced"
     ALL = "all"
-
-
-class ChunkedJSONDataset(torch.utils.data.Dataset):  # type: ignore
-    """A torch-compatible dataset that loads data from one or more JSON files."""
-
-    def __init__(self, root: str) -> None:
-        """Initialise a `ChunkedJSONDataset` instance.
-
-        Params:
-        -------
-        `root`: A path to a single JSON file or a folder containing multiple
-        JSON files (chunks) at its top level.
-
-        Returns:
-        --------
-        None
-        """
-        super().__init__()
-
-        self._root = root
-        self._chunk_cache = None
-
-        self._chunk_map: Dict[str, Tuple[str, ...]] = {}
-        if os.path.isdir(self._root):
-            self._chunk_map = {
-                os.path.join(self._root, name): ()
-                for name in sorted(os.listdir(self._root))
-            }
-        else:
-            self._chunk_map = {self._root: ()}
-
-        # Load top-level JSON keys into `self.chunk_map`
-        for chunk_name in self._chunk_map.keys():
-            with open(chunk_name, "r") as chunk:
-                chunk_data = json.load(chunk)
-                self._chunk_map[chunk_name] = tuple(chunk_data.keys())
-                self._chunk_cache = {chunk_name: chunk_data}
-                del chunk_data
-                print(os.path.relpath(chunk_name, self._root))
-                print(self._chunk_map[chunk_name][:10])
-                print(self._chunk_cache[chunk_name][self._chunk_map[chunk_name][0]])
-
-    def get_chunk_name_and_local_index(self, index: int) -> Tuple[str, int]:
-        """Get the name of the chunk containing the data item at index `index`.
-
-        Params:
-        -------
-        `index`: An index in the range `[0, len(self))`, the index of the data
-        item to retrieve.
-
-        Returns:
-        --------
-        A tuple containing the name of the chunk containing the data item at
-        index `index` and the index of that data item within its chunk.
-        """
-        if index < 0:
-            raise ValueError(f"Parameter {index=} must be greater than or equal to 0")
-
-        cumulative_count = 0
-        for chunk_name, chunk_keys in self._chunk_map.items():
-            if cumulative_count <= index < cumulative_count + len(chunk_keys):
-                return chunk_name, index - cumulative_count
-        raise ValueError(
-            f"Parameter {index=} must be less than or equal to the total ",
-            "number of keys across all chunks",
-        )
-
-    def __getitem__(self, index: int) -> Any:
-        """Get an item from the dataset at a given index."""
-        # Get the name of the chunk the index belongs to and its corresponding
-        # chunk-local index in the range [0, len(self.chunk_map[chunk_name]))
-        chunk_name, local_index = self.get_chunk_name_and_local_index(index)
-
-        # Load the correct chunk into memory if not cached
-        if self._chunk_cache is None or chunk_name not in self._chunk_cache.keys():
-            with open(chunk_name, "r") as chunk:
-                self._chunk_cache = {chunk_name: json.load(chunk)}
-
-        # Get item key and return item
-        item_key = self._chunk_map[chunk_name][local_index]
-        return self._chunk_cache[chunk_name][item_key]
-
-    def __len__(self) -> int:
-        """Get the length of the dataset."""
-        return sum([len(chunk_keys) for chunk_keys in self._chunk_map.values()])
 
 
 class GQAQuestions(torch.utils.data.Dataset):  # type: ignore
