81cf37269e6b43d922519cfab4bebff91977a5cd
Alex <alexmirrington@gmail.com>
2020-08-06 17:32:44 +1000

    Add unit tests for ChunkedHDF5Dataset

--- graphgen/datasets/utilities.py
+++ graphgen/datasets/utilities.py
@@ -2,7 +2,7 @@
 import json
 from pathlib import Path
 from tempfile import TemporaryDirectory
-from typing import Any, Dict, List, Tuple
+from typing import Any, Dict, List, Optional, Tuple
 from uuid import uuid4
 
 import h5py
@@ -113,7 +113,9 @@ class ChunkedJSONDataset(torch.utils.data.Dataset):  # type: ignore
 class ChunkedHDF5Dataset(torch.utils.data.Dataset):  # type: ignore
     """A torch-compatible dataset that loads data from one or more HDF5 files."""
 
-    def __init__(self, root: Path, chunk_map: Dict[str, Tuple[Path, int]]) -> None:
+    def __init__(
+        self, root: Path, chunk_map: Optional[Dict[str, Tuple[Path, int]]] = None
+    ) -> None:
         """Initialise a `ChunkedHDF5Dataset` instance.
 
         Params:
@@ -137,7 +139,7 @@ class ChunkedHDF5Dataset(torch.utils.data.Dataset):  # type: ignore
             raise ValueError(f"Parameter {root=} must point to a non-empty directory.")
 
         self._root = root
-        self._key_to_idx: Dict[str, int] = {}
+        self._key_to_idx: Optional[Dict[str, int]] = {}
         self._chunks: Tuple[Path, ...] = ()
         self._chunk_sizes: Tuple[int, ...] = ()
 
@@ -155,11 +157,8 @@ class ChunkedHDF5Dataset(torch.utils.data.Dataset):  # type: ignore
         for dataset, shape in dataset_shapes.items():
             chunk_start = 0
             for chunk_idx, chunk_size in enumerate(self._chunk_sizes):
-                print(self._chunks[chunk_idx].resolve())
                 source = h5py.VirtualSource(
-                    self._chunks[chunk_idx].resolve(),
-                    dataset,
-                    shape=(chunk_size,) + shape,
+                    self._chunks[chunk_idx], dataset, shape=(chunk_size,) + shape,
                 )
                 layouts[dataset][chunk_start : chunk_start + chunk_size] = source
                 chunk_start += chunk_size
@@ -172,13 +171,17 @@ class ChunkedHDF5Dataset(torch.utils.data.Dataset):  # type: ignore
 
         self._data = h5py.File(self._vds, "r")
 
-        self._key_to_idx = {
-            image_id: sum(
-                [self._chunk_sizes[i] for i in range(self._chunks.index(chunk))]
-            )
-            + idx
-            for image_id, (chunk, idx) in chunk_map.items()
-        }
+        self._key_to_idx = (
+            {
+                image_id: sum(
+                    [self._chunk_sizes[i] for i in range(self._chunks.index(chunk))]
+                )
+                + idx
+                for image_id, (chunk, idx) in chunk_map.items()
+            }
+            if chunk_map is not None
+            else None
+        )
 
     def _load_dataset_metadata(
         self,
@@ -189,17 +192,32 @@ class ChunkedHDF5Dataset(torch.utils.data.Dataset):  # type: ignore
 
         dataset_shapes: Dict[str, Tuple[Any, ...]] = {}
         chunk_sizes: List[int] = []
+        to_remove = []
         for chunk_idx, chunk_name in enumerate(chunks):
             try:
                 with h5py.File(chunk_name, "r") as chunk:
+                    # Get shapes of datasets
                     shape = {
                         dataset: tuple(chunk[dataset].shape) for dataset in chunk.keys()
                     }
+
+                    # Set dataset shapes
                     if len(dataset_shapes) == 0:
                         dataset_shapes = {
                             dataset: val[1:] for dataset, val in shape.items()
                         }
-                        continue
+
+                    # Enforce datasets are the same length in first dimension.
+                    chunk_size = None
+                    for dataset_shape in shape.values():
+                        if chunk_size is None:
+                            chunk_size = dataset_shape[0]
+                            chunk_sizes.append(chunk_size)
+                        elif chunk_size != dataset_shape[0]:
+                            raise ValueError(
+                                f"Chunk {chunk_name} must have datasets of ",
+                                "equal length.",
+                            )
 
                     # Enforce dataset existence and check shapes across chunks
                     for dataset, val in shape.items():
@@ -215,20 +233,12 @@ class ChunkedHDF5Dataset(torch.utils.data.Dataset):  # type: ignore
                                 "dataset in other chunks. Expected shape ",
                                 f"{dataset_shapes[dataset][0]} but got {val[1:]}",
                             )
-
-                    # Enforce datasets are the same length in first dimension.
-                    chunk_size = None
-                    for dataset_shape in shape.values():
-                        if chunk_size is None:
-                            chunk_size = dataset_shape[0]
-                            chunk_sizes.append(chunk_size)
-                        elif chunk_size != dataset_shape[0]:
-                            raise ValueError(
-                                f"Chunk {chunk_name} must have datasets of ",
-                                "equal length.",
-                            )
             except OSError:
-                chunks.remove(chunk_name)
+                to_remove.append(chunk_name)
+
+        for chunk in to_remove:
+            chunks.remove(chunk)
+
         return tuple(chunks), tuple(chunk_sizes), dataset_shapes
 
     def __getitem__(self, index: int) -> Any:
@@ -237,8 +247,15 @@ class ChunkedHDF5Dataset(torch.utils.data.Dataset):  # type: ignore
 
     def __len__(self) -> int:
         """Get the length of the dataset."""
-        return len(self._data)
+        key = list(self._data.keys())[0]
+        return len(self._data[key])
 
     def key_to_index(self, key: str) -> int:
         """Get index of a given key in the dataset."""
-        return self._key_to_idx[key]
+        if self._key_to_idx is None:
+            key_int = int(key)
+            if 0 <= key_int < len(self):
+                return key_int
+        elif key in self._key_to_idx:
+            return self._key_to_idx[key]
+        raise ValueError(f"Parameter {key=} is not a valid key for the dataset.")
