a1176a5e8ec68c9ab7d3e6eb63ad2d3f9decb2fa
Alex <alexmirrington@gmail.com>
2020-08-16 15:48:33 +1000

    Use inheritance over composition for GQAImages

--- graphgen/datasets/gqa/images.py
+++ graphgen/datasets/gqa/images.py
@@ -1,7 +1,6 @@
 """A torch-compatible GQA images dataset implementation."""
-from typing import Any, Callable, Optional
+from typing import Callable, Optional
 
-import torch.utils.data
 from PIL import Image
 from torch import Tensor
 
@@ -9,7 +8,7 @@ from ...config.gqa import GQAFilemap
 from ..utilities import ImageFolderDataset
 
 
-class GQAImages(torch.utils.data.Dataset):  # type: ignore
+class GQAImages(ImageFolderDataset):
     """A torch-compatible dataset that retrieves GQA image samples."""
 
     def __init__(
@@ -22,39 +21,23 @@ class GQAImages(torch.utils.data.Dataset):  # type: ignore
         Params:
         -------
         `filemap`: The filemap to use when determining where to load data from.
-
-        Returns:
-        --------
-        None
         """
-        super().__init__()
         if not isinstance(filemap, GQAFilemap):
             raise TypeError(
                 f"Parameter {filemap=} must be of type {GQAFilemap.__name__}."
             )
 
-        images_root = filemap.image_path()
-        if not images_root.exists():
+        root = filemap.image_path()
+        if not root.exists():
             raise ValueError(
                 f"Parameter {filemap=} does not refer to a valid images directory."
             )
 
+        super().__init__(root, transform)
+
         self._filemap = filemap
-        self._data = ImageFolderDataset(images_root, transform)
 
     @property
     def filemap(self) -> GQAFilemap:
         """Get the dataset's filemap."""
         return self._filemap
-
-    def __len__(self) -> int:
-        """Get the length of the dataset."""
-        return len(self._data)
-
-    def __getitem__(self, index: int) -> Any:
-        """Get an item from the dataset at a given index."""
-        return self._data[index]
-
-    def key_to_index(self, key: str) -> Any:
-        """Get the index of the image in the dataset with a given image id."""
-        return self._data.key_to_index(key)
