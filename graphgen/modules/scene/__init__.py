4ec61c7b4e5ea13749cde27c3975f9f6824cd609
Alex <alexmirrington@gmail.com>
2020-12-16 18:06:06 +1100

    Add identity scene graph module

--- graphgen/modules/scene/__init__.py
+++ graphgen/modules/scene/__init__.py
@@ -3,10 +3,14 @@ from .abstract_scene_graph_module import (
     AbstractSceneGraphModule as AbstractSceneGraphModule,
 )
 from .gcn_scene_graph_module import GCNSceneGraphModule as GCNSceneGraphModule
+from .identity_scene_graph_module import (
+    IdentitySceneGraphModule as IdentitySceneGraphModule,
+)
 from .rnn_scene_graph_module import RNNSceneGraphModule as RNNSceneGraphModule
 
 __all__ = [
     AbstractSceneGraphModule.__name__,
     RNNSceneGraphModule.__name__,
     GCNSceneGraphModule.__name__,
+    IdentitySceneGraphModule.__name__,
 ]
